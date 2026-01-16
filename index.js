c4d0191d13d364fe9d45321e165c0f511275b4ec
maju0029 <maju0029@gmail.com>
2026-01-16 19:25:24 +0100

    changed name on map

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 import express from 'express';
-//import handlebars from 'express-handlebars';
+import handlebars from 'express-handlebars';
 //import renderPage from '.lib/renderPage.js';
 
-//const { engine } = handlebars;
-//const { engine } = require ('express-handlebars');
-
-/*app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './templates');
-
-app.get('/', (req, res) => {
-  res.send('Hello, KinoExpress!');
-});
-
-app.listen(5080);*/
-
+const { engine } = handlebars;
+const { engine } = require ('express-handlebars');
 const app = express();
 const PORT = 5080;
 
-app.use(express.static('public'));
+// Handlebars setup
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+/*app.get('/', (req, res) => {
+  res.send('Hello, KinoExpress!');
+});*/
+
+app.use(express.static('static'));
 app.listen(PORT, () => {
   console.log(`KinoExpress is running at http://localhost:${PORT}`);
 });
