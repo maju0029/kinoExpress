85274e14d6a9f416cfed5295326aa641a6d3f45e
maju0029 <maju0029@gmail.com>
2026-01-16 13:21:07 +0100

    removed unecessary json file and made the page a localserver static site

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+//import handlebars from 'express-handlebars';
+//import renderPage from '.lib/renderPage.js';
 
-const app = express();
+//const { engine } = handlebars;
+//const { engine } = require ('express-handlebars');
+
+/*app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
 
 app.get('/', (req, res) => {
   res.send('Hello, KinoExpress!');
 });
 
-app.listen(5080);
+app.listen(5080);*/
+
+const app = express();
+const PORT = 5080;
+
+app.use(express.static('public'));
+app.listen(PORT, () => {
+  console.log(`KinoExpress is running at http://localhost:${PORT}`);
+});
