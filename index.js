30ccd173064a379b44d7dfd1971d666467e65715
maju0029 <maju0029@gmail.com>
2026-01-20 21:35:25 +0100

    removed old movies page, started on the new one with handlebars to make  it SSR

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import fs from 'fs/promises';
 //import renderPage from '.lib/renderPage.js';
 
 const { engine } = handlebars;
-const { engine } = require ('express-handlebars');
+//const { engine } = require ('express-handlebars');
 const app = express();
 const PORT = 5080;
 
@@ -20,3 +21,37 @@ app.use(express.static('static'));
 app.listen(PORT, () => {
   console.log(`KinoExpress is running at http://localhost:${PORT}`);
 });
+
+app.get("/movies", async (req, res) => {
+  try {
+    const response = await fetch("https://plankton-app-xhkom.ondigitalocean.app/api/movies");
+    const movies = await response.json();
+
+    //console.log(movies); // <-- viktigt första gången
+
+    res.render("movies", {
+      title: "Filmer",
+      movies: movies.data
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Kunde inte hämta filmer");
+  }
+});
+
+/*app.get("/movies", (req, res) => {
+  res.render("movies", {title: "Filmer" , movies: [
+    {id:1, title: "Inception"}, {id:2, title: "The Matrix"}
+  ]})
+})
+
+/*
+{{#if movies.length}}
+  <p>Total Movies: {{movies.length}}</p>
+{{else}}
+  <p>No movies available.</p>
+{{/if}}
+
+till html movies ^
+*/
